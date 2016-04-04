f2fba8a830f5bbced5cbdbe76ec6c51c37b0c5fd
Xavier Jodoin <xavier@jodoin.me>
2016-04-04 17:24:20 -0400

    track many currencies

--- index.js
+++ index.js
@@ -1,108 +1,28 @@
-var autobahn = require('autobahn');
-var moment = require('moment');
+var plnx = require('plnx');
+var tracker = require('./tracker');
+var winston = require('winston');
 var _ = require('lodash');
-var elasticsearch = require('elasticsearch');
-
-var client = new elasticsearch.Client({
-  host: 'elasticsearch.weave.local:9200',
-  log: 'info'
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {
+  'timestamp': true
 });
 
 
-var wsuri = "wss://api.poloniex.com";
-var connection = new autobahn.Connection({
-  url: wsuri,
-  realm: "realm1"
-});
-
-connection.onopen = function(session) {
-  function marketEvent(args, kwargs) {
-    _.each(args,function (element) {
-      var toSend;
-      var data = element.data;
-      if(element.type === 'orderBookModify') {
-        // rate: '0.02516071', amount: '119.22000000'
-        toSend = {
-          index: 'poloniex_btc_eth-'+moment().format('YYYY.MM.DD'),
-          type: data.type,
-          body: {
-            '@timestamp': new Date(),
-            rate: parseFloat(data.rate),
-            amount: parseFloat(data.amount)
-          }
-        };
-      }
-      else  if(element.type === 'newTrade') {
-        // { amount: '1.00000000',
-        //   date: '2016-03-18 20:54:01',
-        //   rate: '0.02490201',
-        //   total: '0.02490201',
-        //   tradeID: '5074296',
-        //   type: 'sell' } }
-        toSend = {
-          index: 'poloniex_btc_eth-'+moment().format('YYYY.MM.DD'),
-          type: data.type,
-          body: {
-            '@timestamp': new Date(),
-            rate: parseFloat(data.rate),
-            amount: parseFloat(data.amount),
-            total: parseFloat(data.total)
-          }
-        };
-      }
-
-      if(toSend) {
-        client.create(toSend, function(error, response) {});
-      }
-
-    });
+plnx.return24Volume({}, function(err, data) {
+  if (err) {
+    winston.error(err);
+    return;
   }
-  // [ 'BTC_ETH',
-  //   '0.03116279',
-  //   '0.03116248',
-  //   '0.03115600',
-  //   '0.00525291',
-  //   '29970.17965690',
-  //   '940939.75068520',
-  //   0,
-  //   '0.03319990',
-  //   '0.03029000' ]
-  // currencyPair, last, lowestAsk, highestBid, percentChange, baseVolume, quoteVolume, isFrozen, 24hrHigh, 24hrLow
-  function tickerEvent(args, kwargs) {
+  var toTracks = _.pickBy(data, function(value, key) {
+    return key.indexOf('BTC_') === 0;
+  });
 
-    if(args[0] === 'BTC_ETH') {
-      // [logstash-]YYYY.MM.DD
-      client.create({
-        index: 'poloniex_btc_eth-'+moment().format('YYYY.MM.DD'),
-        type: 'ticker',
-        body: {
-          '@timestamp': new Date(),
-          last: parseFloat(args[1]),
-          lowestAsk: parseFloat(args[2]),
-          highestBid: parseFloat(args[3]),
-          percentChange: parseFloat(args[4]),
-          baseVolume: parseFloat(args[5]),
-          quoteVolume: parseFloat(args[6]),
-          isFrozen: args[7],
-          '24hrHigh': parseFloat(args[8]),
-          '24hrLow': parseFloat(args[9])
-        }
-      }, function(error, response) {
-      });
+  _.forEach(toTracks, function(track, key) {
+    if (track.BTC > 100) {
+      winston.info('Launch tracker for ' + key);
+      tracker.track(key);
     }
 
-  }
+  });
 
-  function trollboxEvent(args, kwargs) {
-    console.log(args);
-  }
-  session.subscribe('BTC_ETH', marketEvent);
-  session.subscribe('ticker', tickerEvent);
-  //session.subscribe('trollbox', trollboxEvent);
-};
-
-connection.onclose = function() {
-  console.log("Websocket connection closed");
-};
-
-connection.open();
+});
--- test.js
+++
@@ -1,4 +0,0 @@
-var moment = require('moment');
-
-var result = moment().format('YYYY.MM.DD');
-console.log(result);
